51b4751ecb51df0988763b3a16db33d5cdb6c288
hayden11hi <61327358+hayden11hi@users.noreply.github.com>
2025-10-17 11:44:03 +0100

    Update sort-play-loader.mjs

--- sort-play-loader.mjs
+++ sort-play-loader.mjs
@@ -1,6 +1,7 @@
-fetch("https://api.github.com/repos/hoeci/sort-play/contents/sort-play.js")
+fetch("https://api.github.com/repos/drizzy-solutions/sort-play/contents/sort-play.js")
     .then(res => res.json())
     .then(json => atob(json.content))
     .then(content => new Blob([content], { type: "application/javascript" }))
     .then(URL.createObjectURL)
+
     .then(url => import(url))
